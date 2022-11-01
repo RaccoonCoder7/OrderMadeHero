c834dccaca4e9addef87089c6e691bcb9b828227
RC <RaccoonCoder7@gmail.com>
2022-11-01 19:02:35 +0900

    - 시작씬 추가 - 초기세팅씬 추가 - 임시텍스쳐 추가 - DOTween 모듈 추가

---
+++ FWB/Assets/04_Scripts/StartSceneMgr.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneMgr : MonoBehaviour
+{
+    public Button startBtn;
+    public Button continueBtn;
+    public Button bookBtn;
+    public Button settingBtn;
+    public Button quitBtn;
+    public Button alertDodgeBtn;
+    public GameObject alertPanel;
+
+
+    void Start()
+    {
+        alertPanel.SetActive(false);
+        startBtn.onClick.AddListener(ChangeToInitScene);
+        continueBtn.onClick.AddListener(OpenAlertPanel);
+        bookBtn.onClick.AddListener(OpenAlertPanel);
+        settingBtn.onClick.AddListener(OpenAlertPanel);
+        quitBtn.onClick.AddListener(Quit);
+        alertDodgeBtn.onClick.AddListener(() => alertPanel.SetActive(false));
+    }
+
+    private void ChangeToInitScene()
+    {
+        SceneManager.LoadScene("InitScene");
+    }
+
+    private void OpenAlertPanel()
+    {
+        alertPanel.SetActive(true);
+    }
+
+    private void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
