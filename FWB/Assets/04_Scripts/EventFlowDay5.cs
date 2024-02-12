60a3e4197a2ed4ec1243ffd59c6aa91f135e3d6b
Reach <RaccoonCoder7@gmail.com>
2024-02-12 15:04:15 +0900

    [43] - 5일차 연출 기능 추가 - 에러코드 임시 주석처리

--- FWB/Assets/04_Scripts/EventFlowDay5.cs
+++ FWB/Assets/04_Scripts/EventFlowDay5.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 /// <summary>
@@ -5,6 +6,8 @@ using UnityEngine;
 /// </summary>
 public class EventFlowDay5 : EventFlow
 {
+    private GameObject puffet;
+
     public override void StartFlow()
     {
         mgr.StartText("Day5_1", EndDay5_1Routine, EndDay5_1Routine);
@@ -13,7 +16,50 @@ public class EventFlowDay5 : EventFlow
     private void EndDay5_1Routine()
     {
         mgr.EndText();
-        mgr.mainChatPanel.SetActive(false);
+        puffet = mgr.imageList.Find(x => x.key.Equals("화난퍼펫")).imageObj;
+        puffet.SetActive(true);
+        puffet.transform.DOLocalMoveX(-430, 1).SetEase(Ease.OutBack).OnComplete(() =>
+        {
+            mgr.StartText("Day5_2", EndDay5_2Routine);
+        });
+    }
+
+    private void EndDay5_2Routine()
+    {
+        mgr.EndText();
+
+        var navi = mgr.imageList.Find(x => x.key.Equals("당황한나비")).imageObj;
+        navi.transform.DOLocalMoveX(100, 1).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            navi.SetActive(false);
+        });
+
+        var puffetRectTr = puffet.GetComponent<RectTransform>();
+        var size = puffetRectTr.sizeDelta;
+        DOTween.To(() => size.x, x =>
+        {
+            size.x = x;
+            puffetRectTr.sizeDelta = size;
+        }, 1300, 1);
+        puffet.transform.DOLocalMoveX(0, 1).SetEase(Ease.OutCubic).OnComplete(() =>
+        {
+            mgr.StartText("Day5_3", EndDay5_3Routine);
+        });
+    }
+
+    private void EndDay5_3Routine()
+    {
+        mgr.EndText();
+        CommonTool.In.PlayOneShot("crash");
+        puffet.transform.DOShakePosition(1, new Vector3(38, 25, 0), 50).OnComplete(() =>
+        {
+            mgr.StartText("Day5_4", EndDay5_4Routine);
+        });
+    }
+
+    private void EndDay5_4Routine()
+    {
+        mgr.EndText();
         mgr.pcChatPanel.SetActive(false);
 
         StartCoroutine(mgr.StartNormalRoutine(8, mgr.EndNormalOrderRoutine));
