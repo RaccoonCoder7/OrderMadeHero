34f244f175327535627f2de21a95b02e3f4734a6
LAPILA <lapila3004@gmail.com>
2024-06-02 02:17:07 +0900

    53 - UI visual Update

--- FWB/Assets/04_Scripts/Settings.cs
+++ FWB/Assets/04_Scripts/Settings.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button autoTextOnButton;
+    [SerializeField] private Button autoTextOffButton;
+    [SerializeField] private Button closeSettingsButton;
+    [SerializeField] private GameObject settingsMenu;
+
+    [SerializeField] private Button textSpeedSlowButton;
+    [SerializeField] private Button textSpeedMidButton;
+    [SerializeField] private Button textSpeedFastButton;
+
+
     void Start()
     {
-        
+        // 버튼에 이벤트 리스너 연결
+        autoTextOnButton.onClick.AddListener(EnableAutoText);
+        autoTextOffButton.onClick.AddListener(DisableAutoText);
+        closeSettingsButton.onClick.AddListener(CloseSettings);
+
+        textSpeedSlowButton.onClick.AddListener(SetTextSpeedSlow);
+        textSpeedMidButton.onClick.AddListener(SetTextSpeedMid);
+        textSpeedFastButton.onClick.AddListener(SetTextSpeedFast);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnableAutoText()
     {
-        
+    }
+
+    private void DisableAutoText()
+    {
+    }
+
+    private void SetTextSpeedSlow()
+    {
+    }
+
+    private void SetTextSpeedMid()
+    {
+    }
+
+    private void SetTextSpeedFast()
+    {
+    }
+
+    private void CloseSettings()
+    {
+        // 설정 메뉴 비활성화
+        settingsMenu.SetActive(false);
     }
 }
