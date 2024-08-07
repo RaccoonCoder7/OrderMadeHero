57e120a028082c2d5f446a28471fcb1583e4001e
LAPILA <lapila3004@gmail.com>
2024-08-07 21:09:44 +0900

    BossBattle 미완성 ver
    BossBattle 구현 된 것
    성향치에 따라 보스가 결정
    총 3회 실패하면 보스전이 실패하여 보스전을 다시 시작하게 된다.
    실패할 때 마다 왼쪽 상단 캐릭터 팝업의 UI와 모습이 균열이 가기 시작한다.
    제작 성공시 화면이 흔들리며 상대방에게 공격이 성공한 연출 오른쪽 상단
    마지막 청사진은 특수약점을 실패하면 앞선 제작 실패 횟수와 상관없이 게임오버 보스전을 다시 시작하게 된다.
    보상으로 우호 성향치 200, 명성치 200, 크레딧 1만, 보스 캐릭터의 녹아웃(부위파괴) 일러스트를 획득 할 수 있다
    보스 기믹

---
+++ FWB/Assets/04_Scripts/BossBattleManager.cs
@@ -0,0 +1,411 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BossBattleManager : MonoBehaviour
+{
+    [Header("Managers")]
+    public PuzzleMgr puzzleMgr;
+    public GameMgr gameMgr;
+
+    [Header("UI")]
+    public RectTransform gageRectTr;
+    public Image bossImage;
+    public Text teamDialogue;
+    public Image allyImage;
+    public Button clearPuzzleButton;
+    public Button failPuzzleButton;
+    public GameObject screenShakeTarget;
+    public RectTransform dialogueBox;
+    public Transform screenTarget;
+    public Transform chipsetPanel;
+    public Canvas gameCanvas;
+
+    [Header("Sprites")]
+    public Sprite puppetSprite;
+    public Sprite bunnyNormalSprite;
+    public Sprite bunnyCrackedSprite;
+    public Sprite bunnyBrokenSprite;
+
+    [Header("Dialogue Pos")]
+    public float dialogueBoxHiddenX = -1350f;
+    public float dialogueBoxVisibleX = -590f;
+
+    [Header("Game Setting")]
+    public float maxTime = 60f;
+    public int maxPuzzleCnt = 5;
+
+    private bool isPuzzleCompleted;
+    private bool isGamePlaying;
+    private int succeedPuzzleCnt;
+    private int failureCount;
+    private float timer;
+    private int currentPuzzleIndex;
+    private bool isHero;
+    private float initialGageWidth;
+    private List<Color> originalChipColors;
+    private Vector2 initialGagePos;
+
+    private void Start()
+    {
+        Initialize();
+        StartCoroutine(StartBossBattle());
+    }
+
+    private void Update()
+    {
+        if (isGamePlaying)
+        {
+            UpdateTimer();
+        }
+    }
+
+    private void Initialize()
+    {
+        initialGageWidth = gageRectTr.sizeDelta.x;
+        initialGagePos = gageRectTr.anchoredPosition;
+        SaveOriginalChipColors();
+        clearPuzzleButton.onClick.AddListener(() => ProcessPuzzleResult(3));
+        failPuzzleButton.onClick.AddListener(() => ProcessPuzzleResult(1));
+    }
+
+    private void SaveOriginalChipColors()
+    {
+        originalChipColors = new List<Color>();
+        var images = chipsetPanel.GetComponentsInChildren<Image>();
+        foreach (var image in images)
+        {
+            originalChipColors.Add(image.color);
+        }
+    }
+
+    private void RestoreOriginalChipColors()
+    {
+        var images = chipsetPanel.GetComponentsInChildren<Image>();
+        for (int i = 0; i < images.Length; i++)
+        {
+            images[i].color = originalChipColors[i];
+        }
+    }
+
+    private void DetermineBossAndAlly()
+    {
+        if (gameMgr.tendency >= 0)
+        {
+            isHero = true;
+            allyImage.sprite = bunnyNormalSprite;
+            bossImage.sprite = puppetSprite;
+        }
+        else
+        {
+            isHero = false;
+            allyImage.sprite = puppetSprite;
+            bossImage.sprite = bunnyNormalSprite;
+        }
+    }
+
+    private void SetTableDatasForBossBattle()
+    {
+        foreach (var chip in gameMgr.chipTable.chipList)
+        {
+            chip.createEnable = true;
+        }
+
+        foreach (var bpc in gameMgr.weaponDataTable.bluePrintCategoryList)
+        {
+            if (bpc.categoryKey.Equals("t_sword") || bpc.categoryKey.Equals("t_blunt"))
+            {
+                foreach (var bp in bpc.bluePrintList)
+                {
+                    bp.createEnable = true;
+                }
+            }
+        }
+    }
+
+    private IEnumerator StartBossBattle()
+    {
+        DetermineBossAndAlly();
+        SetTableDatasForBossBattle();
+        ResetGameState();
+
+        puzzleMgr.isFeverMode = true;
+        puzzleMgr.isTutorial = false;
+        isGamePlaying = true;
+
+        yield return ShowDialogue("보스 전투 시작!");
+
+        while (currentPuzzleIndex < maxPuzzleCnt)
+        {
+            StartNewBossBattlePuzzle();
+            puzzleMgr.OnMakingDone += OnBossBattleMakingDone;
+
+            isPuzzleCompleted = false;
+            yield return new WaitUntil(() => isPuzzleCompleted);
+
+            ApplyBossGimmick();
+            yield return ShowGimmickDialogue();
+
+            puzzleMgr.OnMakingDone -= OnBossBattleMakingDone;
+
+            currentPuzzleIndex++;
+        }
+
+        EndBossBattle(true);
+    }
+
+    private void StartNewBossBattlePuzzle()
+    {
+        var blueprint = GetRandomBlueprint();
+        gameMgr.currentBluePrint = blueprint;
+        puzzleMgr.StartFeverModePuzzle();
+    }
+
+    private WeaponDataTable.BluePrint GetRandomBlueprint()
+    {
+        var orderableBlueprintList = new List<WeaponDataTable.BluePrint>();
+        foreach (var bpc in gameMgr.weaponDataTable.bluePrintCategoryList)
+        {
+            foreach (var bp in bpc.bluePrintList)
+            {
+                if (bp.createEnable)
+                {
+                    orderableBlueprintList.Add(bp);
+                }
+            }
+        }
+
+        int index = Random.Range(0, orderableBlueprintList.Count);
+        return orderableBlueprintList[index];
+    }
+
+    private void ApplyBossGimmick()
+    {
+        Debug.Log($"Applying Boss Gimmick: currentPuzzleIndex = {currentPuzzleIndex}");
+        if (isHero)
+        {
+            if (currentPuzzleIndex == 1)
+            {
+                Debug.Log("Hero: Bunny Gimmick - Hide Chipset Info");
+                HideChipsetInfo();
+            }
+            else if (currentPuzzleIndex == 3)
+            {
+                Debug.Log("Hero: Bunny Gimmick - Invert Screen");
+                InvertScreen();
+            }
+        }
+        else
+        {
+            if (currentPuzzleIndex == 1)
+            {
+                Debug.Log("Villain: Puppet Gimmick - Reduce Time to 45s");
+                maxTime = 45f;
+                timer = maxTime;
+                UpdateGage();
+            }
+            else if (currentPuzzleIndex == 3)
+            {
+                Debug.Log("Villain: Puppet Gimmick - Reduce Time to 30s");
+                maxTime = 30f;
+                timer = maxTime;
+                UpdateGage();
+            }
+        }
+    }
+
+    private IEnumerator ShowGimmickDialogue()
+    {
+        if (isHero)
+        {
+            if (currentPuzzleIndex == 1)
+            {
+                yield return ShowDialogue("버니: 이런 시야가 가려진다!");
+            }
+            else if (currentPuzzleIndex == 3)
+            {
+                yield return ShowDialogue("버니: 위아래가 뒤집혀진다 조심해!");
+            }
+        }
+        else
+        {
+            if (currentPuzzleIndex == 1)
+            {
+                yield return ShowDialogue("퍼펫: 제작 시간을 감소시켰군요..");
+            }
+            else if (currentPuzzleIndex == 3)
+            {
+                yield return ShowDialogue("퍼펫: 또 다시 시간을 감소시킨다라..");
+            }
+        }
+    }
+
+    private void OnBossBattleMakingDone(int result)
+    {
+        if (!isGamePlaying) return;
+
+        if (result == 3)
+        {
+            succeedPuzzleCnt++;
+            ScreenShake();
+            StartCoroutine(ShowDialogue(isHero ? "버니: 공격 꽤나 효과적이야!" : "퍼펫: 이 무기는 효과적이군요.."));
+        }
+        else
+        {
+            failureCount++;
+            UpdateCharacterPopup();
+            StartCoroutine(ShowDialogue(isHero ? "버니: 이런...효과가 없어.. 다시 제대로 만들어줘!!" : "퍼펫: 이런 쓰잘데기 없는 걸...!! 정신차리시지요.."));
+            if (failureCount >= 3)
+            {
+                EndBossBattle(false);
+            }
+        }
+        isPuzzleCompleted = true;
+    }
+
+    private void UpdateCharacterPopup(bool reset = false)
+    {
+        if (!isHero) return;
+        if (reset)
+        {
+            allyImage.sprite = bunnyNormalSprite;
+        }
+        else
+        {
+            switch (failureCount)
+            {
+                case 1:
+                    allyImage.sprite = bunnyCrackedSprite;
+                    break;
+                case 2:
+                    allyImage.sprite = bunnyBrokenSprite;
+                    break;
+            }
+        }
+    }
+
+    private void ProcessPuzzleResult(int result)
+    {
+        if (!isGamePlaying) return;
+        OnBossBattleMakingDone(result);
+    }
+
+    private void ScreenShake()
+    {
+        screenShakeTarget.transform.DOShakePosition(1, new Vector3(20, 20, 0), 20, 90, false, true);
+    }
+
+    private void EndBossBattle(bool success)
+    {
+        isGamePlaying = false;
+        puzzleMgr.isFeverMode = false;
+
+        if (success)
+        {
+            teamDialogue.text = "보스 처치 성공";
+            CommonTool.In.OpenAlertPanel("보스 처치 성공! 보상: 우호 성향치 200, 명성치 200, 크레딧 1만");
+
+            gameMgr.fame += 200;
+            gameMgr.credit += 10000;
+            if (isHero)
+            {
+                gameMgr.tendency += 200;
+            }
+            else
+            {
+                gameMgr.tendency -= 200;
+            }
+        }
+        else
+        {
+            teamDialogue.text = "보스 처치 실패...";
+            CommonTool.In.OpenAlertPanel("보스 처치 실패... 보스전을 다시 시작합니다.");
+        }
+
+        ResetGameState();
+        ResetScreen();
+        StartCoroutine(StartBossBattle());
+    }
+
+    private void ResetGameState()
+    {
+        maxTime = 60f;
+        timer = maxTime;
+        failureCount = 0;
+        succeedPuzzleCnt = 0;
+        currentPuzzleIndex = 0;
+        isPuzzleCompleted = false;
+        RestoreOriginalChipColors();
+        UpdateGage();
+        UpdateCharacterPopup(true);
+    }
+
+    private void UpdateTimer()
+    {
+        timer -= Time.deltaTime;
+        UpdateGage();
+        if (timer <= 0)
+        {
+            EndBossBattle(false);
+        }
+    }
+
+    private void UpdateGage()
+    {
+        float gageWidth = initialGageWidth * (timer / maxTime);
+        Vector2 sizeDelta = gageRectTr.sizeDelta;
+        sizeDelta.x = gageWidth;
+        gageRectTr.sizeDelta = sizeDelta;
+        gageRectTr.anchoredPosition = new Vector2(initialGagePos.x - (initialGageWidth - gageWidth) / 2, initialGagePos.y);
+    }
+
+    private IEnumerator ShowDialogue(string message)
+    {
+        isGamePlaying = false;
+        ToggleCanvasInteractable(false);
+        teamDialogue.text = message;
+        ShowDialogueBox();
+        yield return new WaitForSeconds(3f);
+        HideDialogueBox();
+        ToggleCanvasInteractable(true);
+        isGamePlaying = true;
+    }
+
+    private void ToggleCanvasInteractable(bool isInteractable)
+    {
+        foreach (var button in gameCanvas.GetComponentsInChildren<Button>())
+        {
+            button.interactable = isInteractable;
+        }
+    }
+
+    private void ShowDialogueBox()
+    {
+        dialogueBox.DOLocalMoveX(dialogueBoxVisibleX, 1f).SetEase(Ease.OutQuad);
+    }
+
+    private void HideDialogueBox()
+    {
+        dialogueBox.DOLocalMoveX(dialogueBoxHiddenX, 1f).SetEase(Ease.InQuad);
+    }
+
+    private void ResetScreen()
+    {
+        screenTarget.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void InvertScreen()
+    {
+        screenTarget.rotation = Quaternion.Euler(0, 0, 180);
+    }
+
+    private void HideChipsetInfo()
+    {
+        foreach (var image in chipsetPanel.GetComponentsInChildren<Image>())
+        {
+            image.color = new Color(0, 0, 0, 0);
+        }
+    }
+}
